cdc41a0ecb9ed60724d993bbca8381e61ea03978
Unknownmaster0 <sagar192002singh@gmail.com>
2023-09-17 20:45:18 +0530

    this is demo to learn the github

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b){
+    return a+b;
+}
+
+int main()
+{
+    cout<<"this is sagar singh"<<endl;
+    int a = 4;
+    int b = 6;
+    cout<<"the sum is -> "<<sum(a,b)<<endl;
+    return 0;
+}
